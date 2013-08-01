bd9d643dc3856664fb42386e88d50a4b94009c84
jknebel <jknebel.prestataire@vsc-technologies.com>
2013-08-01 19:23:38 +0200

    wip

---
+++ genereate-readme.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+for FILE in $(find Snippets/ -name *.tmSnippet); 
+do 
+    snip=$(gawk -vRS="</string>" '/<key>/{gsub(/<key>|<string>|\n| +/,"") ; sub("</key>","⇥") ;print}' $FILE | grep -e "name\|tabTrigger" | tr -d \\t && echo "===")
+    echo -e "$snip\n" >> readme.md;
+done
